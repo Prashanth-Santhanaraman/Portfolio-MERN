e8301ff7aa643a6ff823d4b746ae412cf581e8d4
Prashanth <73587553+Prashanth-Santhanaraman@users.noreply.github.com>
2026-09-11 13:01:57 +0530

    Change in CORS Policy

--- Backend/Server.js
+++ Backend/Server.js
@@ -28,9 +28,8 @@ app.use(express.json());
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to PRASHANTH portfolio backend");
